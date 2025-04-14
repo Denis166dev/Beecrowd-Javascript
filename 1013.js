e3efd076c645f89041e46a1354209ea3f400b796
Denis Willian <161047038+Denis166dev@users.noreply.github.com>
2025-04-14 12:59:47 +0000

    Questão 1013

---
+++ 1013.js
@@ -0,0 +1,5 @@
+let input = require('fs').readFileSync('/dev/stdin', 'utf8');
+let [A, B, C] = input.split(" ").map(item => parseInt(item));
+let maiorAB = (A + B + Math.abs(A - B)) / 2;
+let maiorXC = (C + maiorAB + Math.abs(C - maiorAB)) / 2;
+console.log(maiorXC + " eh o maior");
