5093d431eb3207a85f349d1746db259e2e09c9ed
Denis Willian <161047038+Denis166dev@users.noreply.github.com>
2025-04-14 12:57:37 +0000

    Questão 1011

---
+++ 1011.js
@@ -0,0 +1,5 @@
+let input = require('fs').readFileSync('/dev/stdin', 'utf8');
+let raio = parseFloat(input);
+let pi =  3.14159; 
+let volume = (4/3) * pi * Math.pow(raio, 3);
+console.log("VOLUME = " + volume.toFixed(3));
