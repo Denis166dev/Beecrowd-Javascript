363870f9c029b9948912648ade11fccafa71c9c3
Denis Willian <161047038+Denis166dev@users.noreply.github.com>
2025-04-14 13:01:14 +0000

    Questão 1015

---
+++ 1015.js
@@ -0,0 +1,6 @@
+let input = require('fs').readFileSync('/dev/stdin', 'utf8');
+let lines = input.split('\n');
+let [x1, y1] = lines[0].split(" ");
+let [x2, y2] = lines[1].split(" ");
+let distancia = Math.sqrt(Math.pow( (x2 - x1), 2) + Math.pow((y2 - y1), 2));
+console.log(distancia.toFixed(4));
