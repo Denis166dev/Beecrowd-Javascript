332f78b9e8a021fa3a97552539de9de96d503f40
Denis Willian <161047038+Denis166dev@users.noreply.github.com>
2025-04-18 23:44:10 +0000

    Questão 1035

---
+++ 1035.js
@@ -0,0 +1,11 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split(' ');
+let A = Number(lines.shift());
+let B = Number(lines.shift());
+let C = Number(lines.shift());
+let D = Number(lines.shift());
+if(B > C && D > A && (C+D) > (A+B) && C >= 0 && D >= 0 && A % 2 === 0) {
+    console.log("Valores aceitos");
+} else {
+    console.log("Valores nao aceitos");
+}
